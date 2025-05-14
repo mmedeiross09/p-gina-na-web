3ed099484fa8133637225a8a795c84b35f898eb5
mmedeiross09 <medeiros.siqueira@escola.pr.gov.br>
2025-05-14 12:21:33 -0300

    14/05

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'artes',
+    'Qual é a importância da originalidade na pintura?',
+    'A originalidade é essencial na pintura, pois é através dela que o artista consegue transmitir sua visão única do mundo. Cada pintor possui um estilo próprio e suas obras são como uma extensão de sua personalidade.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'biologia marinha',
+    'Qual é a origem da biologia marinha?',
+    'Os primeiros estudos de biologia marinha remontam aos fenícios e aos gregos , conhecidos como os primeiros exploradores dos oceanos e da sua composição. As primeiras observações registadas sobre a distribuição e os hábitos da vida marinha foram feitas por Aristóteles (384–322 a.C.).'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'astronomia',
+    'O que aconteceria se a Lua desaparecesse? ',
+    'A Lua influencia a Terra, e sua ausência repentina causaria mudanças climáticas extremas e caos no planeta, mas isso é muito improvável de acontecer em um futuro próximo, pois ela se afasta lentamente e levaria bilhões de anos para causar grandes alterações.'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'fisica',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
